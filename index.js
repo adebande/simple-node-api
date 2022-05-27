38da67dff76ea7e407f8fd4e95ab9e181c636d62
adebande <63793809+adebande@users.noreply.github.com>
2022-05-27 23:14:42 +0200

    Add comments :speech_balloon:

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-
 const Name= require('./models/name.js')
-
-require("dotenv").config();
 const dbconfig= require('./config/db.config')
+require("dotenv").config();
 
+//Connect to database
 mongoose.connect(dbconfig.url);
 
+
+//Check database connection
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to Mongoose")
 });
 
+
+//Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//Route to get all names
 app.get('/names', (req,res) => {
     Name.find(function(err, names){
         if (err){
@@ -27,6 +31,8 @@ app.get('/names', (req,res) => {
     })
 })
 
+
+//Route to post a new name
 app.post('/names', (req,res) => {
     var name = new Name();
       name.name = req.query.name;
@@ -38,6 +44,8 @@ app.post('/names', (req,res) => {
       })
 })
 
+
+//Listen on port 8080
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
     console.log('Listening on http://localhost:'+ PORT)
